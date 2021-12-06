43f922849f13c48586dd70685d0ab8879700576c
Pimenov <pimenovvasiliy@gosniias.ru>
2021-12-07 00:53:19 +0300

    Косметические улучшения в методе Question.ask()

--- common/src/main/java/ru/croccode/hypernull/util/Question.java
+++ common/src/main/java/ru/croccode/hypernull/util/Question.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Question {
     public static boolean ask(String question) {
-        System.out.printf(question.endsWith("?") ? "%s (Y/N)" : "%s? (Y/N)", question);
+        System.out.printf(question.endsWith("?") ? "%s (Y/N)\n" : "%s? (Y/N)\n", question);
         while (true) {
             String answer = new Scanner(System.in).nextLine();
-            if (answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes"))
+            if (answer.trim().equalsIgnoreCase("y") || answer.trim().equalsIgnoreCase("yes"))
                 return true;
-            else if (answer.equalsIgnoreCase("n") || answer.equalsIgnoreCase("no"))
+            else if (answer.trim().equalsIgnoreCase("n") || answer.trim().equalsIgnoreCase("no"))
                 return false;
             else
                 System.out.println("Неизвестный тип ответа. Пожалуйста, попробуйте еще раз.");
